4906b0e2751676a86dcb267b1885a001983c850a
CommanderPho <PhoHale@gmail.com>
2021-09-14 15:09:15 -0400

    updated to modern syntax

--- Package.swift
+++ Package.swift
@@ -1,6 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CHDF5",
-    pkgConfig: "libhdf5"
+	products: [
+		.library(name: "CHDF5", targets: ["CHDF5"])
+	],
+	targets: [
+		.systemLibrary(name: "CHDF5", pkgConfig: "libhdf5",
+					   providers: [.brewItem(["hdf5"])])
+	]
 )
+// pkgConfig: "libhdf5",
